598cea30e0e77b1ac00d202d865d45a02f590c46
Shubham Kumar <k.shubham@olamnet.com>
2025-08-25 14:50:11 +0530

    Initial commit - secure-app project with CI workflow

---
+++ src/test/java/com/company/secureapp/App.java
@@ -0,0 +1,12 @@
+package com.company.secureapp;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+
+    // Method used in AppTest.java
+    public String getGreeting() {
+        return "Hello World!";
+    }
+}
---
+++ src/test/java/com/company/secureapp/CalculatorTest.java
@@ -0,0 +1,20 @@
+package com.company.secureapp;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void testAddition() {
+        Calculator calc = new Calculator();
+        assertEquals(5, calc.add(2, 3));
+    }
+
+    @Test
+    void testSubtraction() {
+        Calculator calc = new Calculator();
+        assertEquals(6, calc.subtract(10, 4));
+    }
+}
+
